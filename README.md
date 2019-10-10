45e47e144209badf60116593ea77db761f2c9b8f
Sam Skjonsberg <sams@allenai.org>
2019-10-10 14:54:04 -0700

    🔥 Add the config, package manifest and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# eslint-config-varnish
-Common linting and formatter definitions for Varnish projects
+# ESLint Config
+
+The files in this directory represent the `eslint` configuration settings
+for Varnish.
+
+They're packaged as such so that they can be published as a separate NPM module,
+`@allenai/@allenai/eslint-config-varnish`. This allows them to be shared easily
+across multiple AI2 projects.
+
+For more information about publishing and using shared `eslint` configuration
+definitions, see [this documentation](https://eslint.org/docs/user-guide/configuring).
+
+## Testing
+
+The configuration expressed in this directory is used by the parent project,
+so you can simply run `yarn lint` in the root of the repository to verify
+things are working.
+
+## Publishing
+
+To publish a new version after making changes, follow these steps:
+
+```bash
+~ npm version major|minor|patch
+~ git push --tags origin master
+~ npm publish
+```
+
+🤘 ⛵️ 🎨
---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * This file includes `eslint` settings that are distributed via the
+ * `@allenai/eslint-config-varnish` package and shared amongst many AI2
+ * projects.
+ *
+ * @see https://eslint.org/docs/user-guide/configuring
+ */
+module.exports = {
+    extends: [ "standard", "plugin:prettier/recommended" ],
+    env: {
+        browser: true,
+        es6: true
+    },
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        sourceType: "module"
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint",
+        "prettier"
+    ],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                printWidth: 100,
+                tabWidth: 4,
+                singleQuote: true,
+                jsxBracketSameLine: true,
+                jsxSingleQuote: false
+            }
+        ],
+        "react/jsx-uses-react": 1,
+        "react/jsx-uses-vars": 1,
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                vars: "all",
+                args: "after-used",
+                ignoreRestSiblings: false,
+                argsIgnorePattern: "^_"
+            }
+        ],
+        "no-useless-constructor": "off",
+        "@typescript-eslint/no-useless-constructor": "error"
+    }
+}
